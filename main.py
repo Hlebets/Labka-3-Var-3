32060cbeaf6e87be13b34b4a25ad67674d0a222c
Hlebets <glib.kobryn@gmail.com>
2023-10-12 00:16:25 +0300

    Project Created

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Лабораторна робота №2 Варіант 3")
+
+print("Гліб Кобрин, ІКСМ-1, Навчальна Група №2")
+
+while True:
+    user_input = input("Введіть довільне ціле число у форматі *****: ")
+
+    if user_input.isdigit():
+
+        digits = [int(digit) for digit in user_input]
+
+        max_digit = max(digits)
+
+        print(" ".join(map(str, digits)))
+
+        print("_" * (len(digits) * 2))
+
+        for i in range(max_digit, 0, -1):
+            row = ""
+            for digit in digits:
+                if digit >= i:
+                    row += "# "
+                else:
+                    row += "  "
+            print(row)
+
+        break
+    else:
+        print("Error: Введене значення не є цілим числом.")
